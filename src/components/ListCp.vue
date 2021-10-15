d1a63eb1acaf563b9e4349e366459a6597968b51
booldook <booldook@gmail.com>
2021-10-15 10:46:16 +0900

    Update TitleBar

--- src/components/ListCp.vue
+++ src/components/ListCp.vue
@@ -1,16 +1,13 @@
 <template>
-  <section class="wrapper list-wrapper">
-    <TitleBar />
-  </section>
+  <section class="wrapper list-wrapper"></section>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
-import TitleBar from "./inc/TitleBar.vue"
 
 export default {
   name: "ListCp",
-  components: { TitleBar },
+  components: {},
   computed: {
     ...mapGetters(["GET_BOOKS"]),
   },
