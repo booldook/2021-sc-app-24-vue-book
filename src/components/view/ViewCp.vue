6527f2799081e7c1a46b3ad073949d9942f65f36
booldook <booldook@gmail.com>
2021-10-18 13:07:17 +0900

    ViewCp Design 완성

--- src/components/view/ViewCp.vue
+++ src/components/view/ViewCp.vue
@@ -1,13 +1,29 @@
 <template>
   <section class="wrapper view-wrapper">
-    <InfoTxt :title="title" :content="content" :styled="{ padding: '1em' }" />
-    <InfoImg :title="title" :src="content" :styled="{ padding: '1em' }" />
+    <h3 class="title">도서 상세 내용</h3>
+    <InfoTxt :title="title" :content="title" :styled="styled" />
+    <InfoTxt :title="title" :content="writer" :styled="styled" />
+    <InfoTxt :title="title" :content="date" :styled="styled" />
+    <InfoTxt :title="title" :content="sale" :styled="styled" />
+    <InfoTxt :title="title" :content="content" :styled="styled" />
+    <InfoImg :title="title" :content="cover" :styled="styled" v-show="cover" />
+    <div v-if="true">
+      <InfoImg :title="title" :content="content" :styled="styled" />
+    </div>
+    <div v-else>
+      <InfoTxt :title="title" :content="content" :styled="styled" />
+    </div>
   </section>
 </template>
 
 <script>
 export default {
   name: "ViewCp",
+  data() {
+    return {
+      styled: { padding: "1em" },
+    };
+  },
 };
 </script>
 
